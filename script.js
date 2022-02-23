0a410e1828e4cc4dd2002ff7bfbd646beffdbc2d
GuidoTaboas <guidotaboas@gmail.com>
2022-02-23 12:45:39 +0100

    Refactored the code to use Classes

--- script.js
+++ script.js
@@ -10,160 +10,165 @@ const CLEAR_BUTTON = document.getElementById('clear');
 const DEL_ALL = document.getElementById('deleteAll')
 const MY_LIBRARY = [];
 
-
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-function delAll() {
-    while (BOOK_DISPLAY.firstChild) {
-        BOOK_DISPLAY.removeChild(BOOK_DISPLAY.lastChild);
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 }
 
-function isReadCheck(currentValue, newValue) {
-    if (currentValue.checked) newValue.setAttribute('checked', true);
-}
-
-function createCard(book) {
-    // Create Book card
-    let card = document.createElement('div');
-    card.className = 'bookCard';
-
-    // Add Title from book object
-    let title = document.createElement('span');
-    title.innerText = `Title: ${book.title}`;
-
-
-    // Add author from book object
-    let author = document.createElement('span');
-    author.innerText = `Author: ${book.author}`;
-
-    // Add number of pages from book object
-    let pages = document.createElement('span');
-    pages.innerText = `Number of pages: ${book.pages}`;
-
-    // Dynamic ID naming for each book's checkbox
-    let checkboxId = `checkbox_${book.title.toLowerCase().split(' ').join('_')}`;
-
-    // Create read checkbox and label
-    let isReadDiv = document.createElement('div');
-    let isRead = document.createElement('input');
-    let isReadLabel = document.createElement('label');
-    isReadDiv.className = 'isReadDiv';
-    isReadLabel.for = checkboxId;
-    isReadLabel.innerText = 'Read';
-    isRead.className = 'isReadCheckbox';
-    isRead.id = checkboxId;
-    isRead.type = 'checkbox';
-
-    //Check the value of the checkbox to give the same value to the new card checkbox
-    isReadCheck(IS_READ, isRead);
+class Display {
+    createCard(book) {
+        // Create Book card
+        let card = document.createElement('div');
+        card.className = 'bookCard';
     
-    //Create delete button
-    let delBook= document.createElement('div');
-    delBook.innerText = 'Remove';
-    delBook.className = 'delBook';
-
-    //Appen every element to the main card
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(isReadDiv);
-    isReadDiv.appendChild(isReadLabel);
-    isReadDiv.appendChild(isRead);
-    card.appendChild(delBook);
-
-    setCardColor(isRead, card)
+        // Add Title from book object
+        let title = document.createElement('span');
+        title.innerText = `Title: ${book.title}`;
     
     
+        // Add author from book object
+        let author = document.createElement('span');
+        author.innerText = `Author: ${book.author}`;
+    
+        // Add number of pages from book object
+        let pages = document.createElement('span');
+        pages.innerText = `Number of pages: ${book.pages}`;
+    
+        // Dynamic ID naming for each book's checkbox
+        let checkboxId = `checkbox_${book.title.toLowerCase().split(' ').join('_')}`;
+    
+        // Create read checkbox and label
+        let isReadDiv = document.createElement('div');
+        let isRead = document.createElement('input');
+        let isReadLabel = document.createElement('label');
+        isReadDiv.className = 'isReadDiv';
+        isReadLabel.for = checkboxId;
+        isReadLabel.innerText = 'Read';
+        isRead.className = 'isReadCheckbox';
+        isRead.id = checkboxId;
+        isRead.type = 'checkbox';
+    
+        //Check the value of the checkbox to give the same value to the new card checkbox
+        display.isReadCheck(IS_READ, isRead);
+        
+        //Create delete button
+        let delBook= document.createElement('div');
+        delBook.innerText = 'Remove';
+        delBook.className = 'delBook';
+    
+        //Appen every element to the main card
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(isReadDiv);
+        isReadDiv.appendChild(isReadLabel);
+        isReadDiv.appendChild(isRead);
+        card.appendChild(delBook);
+    
+        display.setCardColor(isRead, card)
+        
+        
+    
+        return card;
+    }  
 
-    return card;
-}    
+    displayLibrary(bookDisplay, MY_LIBRARY) {
+        MY_LIBRARY.forEach(function(book) {
+            let card = display.createCard(book);
+            bookDisplay.appendChild(card);
+        });
+    }
 
-function displayLibrary(bookDisplay, MY_LIBRARY) {
-    MY_LIBRARY.forEach(function(book) {
-        let card = createCard(book);
+    isReadCheck(currentValue, newValue) {
+        if (currentValue.checked) newValue.setAttribute('checked', true);
+    }
+
+    displayBook(bookDisplay, book) {
+        let card = display.createCard(book);
         bookDisplay.appendChild(card);
-    });
-}
-
-function displayBook(bookDisplay, book) {
-    let card = createCard(book);
-    bookDisplay.appendChild(card);
-}
-
-function delButtons() {
-    document.querySelectorAll('.delBook').forEach(button => {
-        button.addEventListener('click', function() {
-        delBook(button);
-    })
-})}
-
-function setCardColor(item, bookCard) {
-    if(item.checked) {
-        bookCard.style.backgroundColor = '#c0ecc0'; 
-        bookCard.style.border = '0.1em solid #c0ecc0';
-    } else {
-        bookCard.style.backgroundColor = '#ffc397'; 
-        bookCard.style.border = '0.1em solid #ffc397';
     }
-}
 
-function checkbox() {
-    document.querySelectorAll('.isReadCheckbox').forEach(button => {
-        button.addEventListener('change', function() {
-            let bookCard = button.parentElement.parentElement
-            setCardColor(button, bookCard)
+    delButtons() {
+        document.querySelectorAll('.delBook').forEach(button => {
+            button.addEventListener('click', function() {
+            display.delBook(button);
         })
-    })
+    })}
+
+    setCardColor(item, bookCard) {
+        if(item.checked) {
+            bookCard.style.backgroundColor = '#c0ecc0'; 
+            bookCard.style.border = '0.1em solid #c0ecc0';
+        } else {
+            bookCard.style.backgroundColor = '#ffc397'; 
+            bookCard.style.border = '0.1em solid #ffc397';
+        }
+    }
+
+    checkbox() {
+        document.querySelectorAll('.isReadCheckbox').forEach(button => {
+            button.addEventListener('change', function() {
+                let bookCard = button.parentElement.parentElement
+                display.setCardColor(button, bookCard)
+                console.log('change')
+            })
+        })
+    }
+
+    delBook(elem) {
+        let book = elem.parentNode;
+        book.parentNode.removeChild(book);
+        MY_LIBRARY.pop(book.childNodes[1].value);
+    }
+    
 }
 
-function delBook(elem) {
-    let book = elem.parentNode;
-    book.parentNode.removeChild(book);
-    MY_LIBRARY.pop(book.childNodes[1].value);
+class Menu {
+    addBook() {
+        if (TITLE.value == '' || AUTHOR.value == '' || PAGES.value == '' || MY_LIBRARY.some(book => book.title === TITLE.value)) {
+            alert('The book you are trying to add is already in the library');
+            return;
+        } else {
+            let newBook = new Book(TITLE.value, AUTHOR.value, PAGES.value, IS_READ.value);
+            MY_LIBRARY.push(newBook);
+            display.displayBook(BOOK_DISPLAY, newBook);
+            display.delButtons();
+            display.checkbox();
+            menu.clearForm();
+        };  
+    }
+
+    clearForm() {
+        TITLE.value = '';
+        AUTHOR.value = '';
+        PAGES.value = '';
+        IS_READ.checked = false;
+    }
+
+    delAll() {
+            while (BOOK_DISPLAY.firstChild) {
+                BOOK_DISPLAY.removeChild(BOOK_DISPLAY.lastChild);
+            }
+        }
 }
 
-function clearForm() {
-    TITLE.value = '';
-    AUTHOR.value = '';
-    PAGES.value = '';
-    IS_READ.checked = false;
-}
+let menu = new Menu();
+let display = new Display();
 
-function addBook() {
-    if (TITLE.value == '' || AUTHOR.value == '' || PAGES.value == '' || MY_LIBRARY.some(book => book.title === TITLE.value)) {
-        console.log('No funciona');
-        return;
-    } else {
-        newBook = new Book(TITLE.value, AUTHOR.value, PAGES.value, IS_READ.value);
-        MY_LIBRARY.push(newBook);
-        displayBook(BOOK_DISPLAY, newBook);
-        delButtons();
-        clearForm();
-    };  
-}
-
-ADD_BOOK_SUBMIT.addEventListener('click', addBook)
-CLEAR_BUTTON.addEventListener('click', clearForm)
-DEL_ALL.addEventListener('click', delAll)
+ADD_BOOK_SUBMIT.addEventListener('click', menu.addBook);
+CLEAR_BUTTON.addEventListener('click', menu.clearForm);
+DEL_ALL.addEventListener('click', menu.delAll);
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
 let theLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', '350', 'read');
 let theNameOfTheWind = new Book('The Name of the Wind', 'Patrick Rothfuss', '450', 'read');
-let theHobbit1 = new Book('The Hobbit1', 'J.R.R. Tolkien', '295', 'not read yet');
-let theLordOfTheRings1 = new Book('The Lord of the Rings1', 'J.R.R. Tolkien', '350', 'read');
-let theNameOfTheWind1 = new Book('The Name of the Wind1', 'Patrick Rothfuss', '450', 'read');
-let theHobbit2 = new Book('The Hobbit2', 'J.R.R. Tolkien', '295', 'not read yet');
-let theLordOfTheRings2 = new Book('The Lord of the Rings2', 'J.R.R. Tolkien', '350', 'read');
-let theNameOfTheWind2 = new Book('The Name of the Win2', 'Patrick Rothfuss', '450', 'read');
 
-MY_LIBRARY.push(theLordOfTheRings, theHobbit, theNameOfTheWind,theLordOfTheRings1, theHobbit1, theNameOfTheWind1,theLordOfTheRings2, theHobbit2, theNameOfTheWind2)
+MY_LIBRARY.push(theLordOfTheRings, theHobbit, theNameOfTheWind);
 
-displayLibrary(BOOK_DISPLAY, MY_LIBRARY);
-delButtons();
-checkbox();
+display.displayLibrary(BOOK_DISPLAY, MY_LIBRARY);
+display.delButtons();
+display.checkbox();
